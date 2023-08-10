0cac0b290ac32e6e26bb30d6e0031abb409a35c9
María Val <mvallej45@gmail.com>
2023-08-09 22:33:17 -0500

    modifica string

--- lista-contactos.js
+++ lista-contactos.js
@@ -1,19 +1,19 @@
-let contacts= [{nombre: "Maria", edad: 15},{nombre: "Carlos", edad: 26},{nombre: "Carolina", edad: 28}];
-const addContact= (contact) => {
-    console.log(contacts.push(contact))
-    return contacts.push()
-}
-addContact({nombre: "Camila", edad: 21});
+let contacts = ["Maria Restrepo", "Carlos Gomez", "Carolina Guzman"];
+const addContact = (contact) => {
+  console.log(contacts.push(contact));
+  return contacts.push();
+};
+addContact("Camila Rodriguez");
 console.log(contacts);
 
-const deleteContact= (a,b) =>{
-    console.log(contacts.splice(a,b))
-    return contacts
-}
-console.log(deleteContact(contacts.length-1,1));
+const deleteContact = (a, b) => {
+  console.log(contacts.splice(a, b));
+  return contacts;
+};
+console.log(deleteContact(contacts.length - 1, 1));
 
-const imprimeContacts= () =>{
-    return contacts
-}
+const imprimeContacts = () => {
+  return contacts;
+};
 
-console.log(imprimeContacts())
+console.log(imprimeContacts());
