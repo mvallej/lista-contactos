7031dabffec43b392d6e7e280b4ee1f7424b592d
María Val <mvallej45@gmail.com>
2023-08-09 22:50:31 -0500

    project list modified

--- lista-contactos.js
+++ lista-contactos.js
@@ -1,9 +1,9 @@
-let contacts= [{nombre: "Maria", edad: 15},{nombre: "Carlos", edad: 26},{nombre: "Carolina", edad: 28}];
+let contacts= [{ id: 76, nombre: "Maria", telefono: 15, ubicacion: "Bogota norte"},{id: 85, nombre: "Carlos", telefono: 26, ubicacion: "Medellin sur-occidente"},{id: 75, nombre: "Carolina", telefono: 28, ubicacion: "Monteria noroccidente"}];
 const addContact= (contact) => {
     console.log(contacts.push(contact))
     return contacts.push()
 }
-addContact({nombre: "Camila", edad: 21});
+addContact({id: 58, nombre: "Camila", telefono: 21,ubicacion: "Barranquilla suroccidente"});
 console.log(contacts);
 
 const deleteContact= (a,b) =>{
